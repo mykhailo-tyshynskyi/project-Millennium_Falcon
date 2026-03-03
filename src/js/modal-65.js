9d3c4bd56949e49a0c9a896cec7d7dc25bb4c37e
Andrii Pavelko <andreypavelko27@gmail.com>
2026-03-04 01:07:52 +0200

    fixed

--- src/js/modal-65.js
+++ src/js/modal-65.js
@@ -1,18 +1,13 @@
 (() => {
-  const refs = {
-
-    openModalBtn: document.querySelector('[data-modal-open-65]'),
-   
-    closeModalBtn: document.querySelector('[data-modal-close-65]'),
   
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-65]'),
+    closeModalBtn: document.querySelector('[data-modal-close-65]'),
     modal: document.querySelector('[data-modal-65]'),
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
-
     refs.modal.classList.toggle('is-open');
   }
 
